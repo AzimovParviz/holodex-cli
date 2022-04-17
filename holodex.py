d5e88024e6fed24ab6b8678968748bfffb1b1cad
Parviz Azimov <parviz.azimov@student.lamk.fi>
2022-04-17 16:22:58 +0300

    basic filtered output done, no option to actually watch the streams. TODO: api key input by option argument and actual implementation of starting the video
    Signed-off-by: Parviz Azimov <parviz.azimov@student.lamk.fi>

--- holodex.py
+++ holodex.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import subprocess
 import requests
 import re
@@ -6,27 +7,33 @@ import os
 
 
 def main(argv):
-    api_key = input("api_key: ")
     url = "https://holodex.net/api/v2/live"
     req_body = url
     try:
-        opts, args = getopt.getopt(argv,"hlu")
+        opts, args = getopt.getopt(argv,"hlo")
     except getopt.GetoptError:
         print ('holodex.py -h for help')
         sys.exit(2)
+        #use argparse instead please
     for opt, arg in opts:
         if opt == '-h':
-            print ('see github or something, maybe i will make a man page in the future lol')
+            print ('-l for language: en,ja,ch,es,ru\
+            -o for organization: Hololive, Nijisanji')
             sys.exit()
         if opt == "-l":
-            language = arg #filter by language
+            limit = arg #limit the output
             print("lang ="+language)
             req_body += "?lang="+language
         if opt == "-o":
-            org = arg #filter by organization
+            org = arg #filter by organization, ex: Hololive, Nijisanji
             print("org ="+org)
             req_body += "?org="+org
-    r = requests.get(req_body)
-    print(r.text)
+    api_key = input("api_key: ")
+    r = requests.get(req_body,headers={'X-APIKEY': '%s' % api_key})
+    data = r.json()
+    for stream in data:
+        print('title: ' + stream['title'])
+        print('name: ' + stream['channel']['name'])
+        
 if __name__ == "__main__":
    main(sys.argv[1:])

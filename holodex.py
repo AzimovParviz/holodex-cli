800210dd7eb917105309b675a3d4d0f185d85d55
Parviz Azimov <parviz.azimov@student.lamk.fi>
2022-04-19 13:57:35 +0300

    additional options: where to watch the stream (youtube or holodex), topic (game_name, karaoke etc)
    Signed-off-by: Parviz Azimov <parviz.azimov@student.lamk.fi>

--- holodex.py
+++ holodex.py
@@ -7,9 +7,10 @@ from datetime import datetime
 
 def main(argv):
     url = "https://holodex.net/api/v2/live?"
-    watch = "https://holodex.net/watch/"
     req_body = url
     parser = argparse.ArgumentParser(description='fetch current live vtubers from holodex')
+    parser.add_argument('--source',
+                        help='where you want to watch the stream: holodex, youtube')
     parser.add_argument('--org',
                         help='filter by organization e.g Hololive, Nijisanji')
     parser.add_argument('-l', type=int,
@@ -18,14 +19,24 @@ def main(argv):
                         help='API key')
     parser.add_argument('--status',
                         help='status of the video: new,upcoming,live,past,missing')
+    parser.add_argument('--topic',
+                        help='topic of the stream (e.g game name, singing')
     args = parser.parse_args()
     r = requests.get(req_body,headers={'X-APIKEY': '%s' % args.key}, params = {'org': args.org, 'limit': args.l, 'status' : args.status})
     data = r.json()
+    if args.source == 'youtube':
+        watch = "https://youtube.com/watch/"
+    else:
+        watch = "https://holodex.net/watch/"
     for stream in data:
         print('title: ' + stream['title'])
-        if stream['channel']['english_name']:
+        try:
+            print('topic: ' + stream['topic_id'])
+        except KeyError:
+            pass
+        try:
             print('name: ' + stream['channel']['english_name'])
-        else:
+        except KeyError:
             print('name: ' + stream['channel']['name'])
         print('starts: ' + str(datetime.strptime(stream['available_at'], '%Y-%m-%dT%H:%M:%S.%fZ')))
         print('url: ' + watch+stream['id']+'\n')
